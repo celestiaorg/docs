870f264d5355534b6452c3531097d6a28835c93d
YazzyYaz <yaz.khoury@gmail.com>
2022-04-11 14:00:43 +0200

    doc: Add docker-compose resources

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,7 @@ const sidebars = {
     { type: "doc", label: "Overview", id: "nodes/overview" },
     { type: "doc", label: "Bridge & Validator Node", id: "nodes/bridge-validator-node" },
     { type: "doc", label: "Light Node", id: "nodes/light-node" },
+    { type: "doc", label: "Devops Resources", id: "nodes/devops-resources"},
   ],
 };
 
