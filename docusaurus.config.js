2fec37d834091d9c7035d25bcdc58584910c224e
Yaz Khoury <yaz@celestia.org>
2022-05-30 23:20:48 +0200

    Update docusaurus.config.js
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/celestiaorg/docs/blob/main/',
+          editUrl: 'https://github.com/celestiaorg/docs/tree/main/',
           routeBasePath: "/",
         },
         theme: {
