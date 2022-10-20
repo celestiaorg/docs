ecc7f2607da05c14911a09ca22e59dd11f8810da
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2022-10-20 17:44:56 -0400

    rm gm files

--- sidebars.js
+++ sidebars.js
@@ -199,46 +199,6 @@ const sidebars = {
       items: [
         {
           type: "category",
-          label: "gm world",
-          link: {
-            type: 'generated-index',
-          },
-          collapsed: true,
-          items: [
-            {
-              type: "doc",
-              label: "gm world Overview",
-              id: "developers/gm-world"
-            },
-            {
-              type: "doc",
-              label: "Setup",
-              id: "developers/gm-setup"
-            },
-            {
-              type: "doc",
-              label: "Run a Light Node",
-              id: "developers/gm-node"
-            },
-            {
-              type: "doc",
-              label: "Build a Sovereign Rollup",
-              id: "developers/gm-rollmint"
-            },
-            {
-              type: "doc",
-              label: "Query your Rollup",
-              id: "developers/gm-query"
-            },
-            {
-              type: "doc",
-              label: "What's next",
-              id: "developers/gm-next"
-            }
-          ]
-        },
-        {
-          type: "category",
           label: "Wordle",
           link: {
             type: 'generated-index',
