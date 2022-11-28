07475f2d17491eed24312c33c9ac933c8b146158
Yaz Khoury <yaz.khoury@gmail.com>
2022-11-28 17:56:36 -0500

    feat: remove fr for debugging

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
   ],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "fr"],
+    locales: ["en"],
   },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
