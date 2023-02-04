0bd43be06a9a962742079f1dc022aff9f9a38ec3
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-03 19:39:11 -0500

    doc: add mocha/arabica versions & use on light-node page

--- docs/nodes/versions.js
+++ docs/nodes/versions.js
@@ -2,5 +2,7 @@ const versions = Object.freeze({
   golang: "1.19.1",
   "app-latest-tag": "v0.1.1",
   "app-latest-sha": "sadf",
+  mochaLatest: "0.6.4",
+  arabicaLatest: "0.6.2",
 });
 export default versions;
