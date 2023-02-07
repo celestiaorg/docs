85f3231394b13195c6701a087c4630bd1710d993
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-07 14:35:29 -0500

    new var for mochaLatestSha

--- docs/nodes/versions.js
+++ docs/nodes/versions.js
@@ -3,6 +3,6 @@ const versions = Object.freeze({
   "celestia-app_latest_tag": "0.11.0",
   "celestia-node_latest_tag_arabica": "0.6.2",
   "celestia-node_latest_tag_mocha": "0.6.4",
-  mochaLatestSha: "sha-747c9e5",
+  "celestia-node_latest_sha_mocha": "sha-747c9e5",
 });
 export default versions;
