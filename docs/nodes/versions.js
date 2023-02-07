8363a625358f395774f714769b08f8a08afae752
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-07 18:10:08 -0500

    Revert "new var for mochaLatestSha"
    This reverts commit 076629d66ee6dcdf2e45fe0abdb8ad0da4895ab4.

--- docs/nodes/versions.js
+++ docs/nodes/versions.js
@@ -1,8 +1,9 @@
 const versions = Object.freeze({
   golang: "1.19.1",
-  "celestia-app_latest_tag": "0.11.0",
-  "celestia-node_latest_tag_arabica": "0.6.2",
-  "celestia-node_latest_tag_mocha": "0.6.4",
-  "celestia-node_latest_sha_mocha": "sha-747c9e5",
+  appLatest: "v0.11.0",
+  mochaLatest: "0.6.4",
+  arabicaLatest: "0.6.2",
+  mochaLatestSha: "sha-747c9e5",
+  "app-latest-sha": "sadf",
 });
 export default versions;
