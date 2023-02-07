0b53911d163c01b7608fe059e722ff09da90203c
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-07 14:31:22 -0500

    change arabicaLatest var

--- docs/nodes/versions.js
+++ docs/nodes/versions.js
@@ -1,9 +1,9 @@
 const versions = Object.freeze({
   golang: "1.19.1",
-  "celestia-app_latest_tag": "v0.11.0",
+  "celestia-app_latest_tag": "0.11.0",
+  "celestia-node_latest_tag_arabica": "0.6.2",
+  "celestia-node_latest_tag_mocha": "0.6.4",
   mochaLatest: "0.6.4",
-  arabicaLatest: "0.6.2",
   mochaLatestSha: "sha-747c9e5",
-  "app-latest-sha": "sadf",
 });
 export default versions;
