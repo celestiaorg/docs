162a443205e425d026e2b08e2aaa940ee0f2deeb
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-14 09:30:15 +0100

    Update arabica_versions.js

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -1,9 +1,9 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v0.11.1",
-  "app-latest-sha": "a1eb594d43137ded5f9c1062168d26139a251e16",
+  "app-latest-tag": "v0.12.7-rc7",
+  "app-latest-sha": "67c90ede03d2543ce9b02726dfe17cf5aad1f20a",
   "core-latest-tag": "v1.14.0-tm-v0.34.23",
   "core-latest-sha": "ab4ef71b202dd9d27c879a820fa06e127d4234fa",
-  "node-latest-tag": "v0.7.0-rc3",
-  "node-latest-sha": "ef59637c0cf3f9f01ff587b37fa842123e641dc7",
+  "node-latest-tag": "v0.7.0-rc4",
+  "node-latest-sha": "8b9937fa04b9702bfb4f4b5604f05aa254713644",
 });
 export default arabicaVersions;
