f306b3947b5edfed6d73438d4d7ed2ba0b30d161
joshcs.eth ᵍᵐ <46639943+jcstein@users.noreply.github.com>
2023-02-17 19:05:08 +0100

    bump node version (#475)

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "67c90ede03d2543ce9b02726dfe17cf5aad1f20a",
   "core-latest-tag": "v1.14.0-tm-v0.34.23",
   "core-latest-sha": "ab4ef71b202dd9d27c879a820fa06e127d4234fa",
-  "node-latest-tag": "v0.7.0-rc5",
-  "node-latest-sha": "dd44463d836d81b60e94182b9e146189cfb5eb2a",
+  "node-latest-tag": "v0.7.0-rc7",
+  "node-latest-sha": "5aee280e4c22208baec5e25490c1a558cad2238f",
 });
 export default arabicaVersions;
