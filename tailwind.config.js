9135fb6ff5146e8d53fe14954a91465c9b60a0cc
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-22 18:13:17 -0500

    style: partial dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/node-gateway-tsx.tsx"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: ["class", '[data-theme="dark"]'],
 };
