799fa5a5a1cadac7a20bbc2f38a9d0d2e1d7bbaa
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-22 18:57:43 -0500

    separate tailwind and custom .css files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/pages/node-api-docs.tsx"],
   theme: {
     extend: {},
   },
