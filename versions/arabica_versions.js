8fc3da088987f3b1f60af7a84485fcf40dd8e3f7
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-03-22 17:26:49 +0100

    fix: remove premature arabica version update

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "1943735bdefc751ae98732760921d49a3534a70a",
   "core-latest-tag": "v1.15.0-tm-v0.34.23",
   "core-latest-sha": "77a1de71feadebaa89c5ecaef0309fd8b9f2f921",
-  "node-latest-tag": "v0.7.2",
-  "node-latest-sha": "eea18a72891af318fb26abb7264416b679ec2a16",
+  "node-latest-tag": "v0.7.1",
+  "node-latest-sha": "c958fd3b3921ca993489d88530e59fe7f337bf4d",
 });
 export default arabicaVersions;
