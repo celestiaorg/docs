483374eadd9706200cd2b5e4093dba7d84c981c1
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-03-27 21:38:24 +0200

    deps: update to v0.8.0-rc0 for bsr

--- versions/blockspacerace_versions.js
+++ versions/blockspacerace_versions.js
@@ -3,7 +3,7 @@ const blockspaceraceVersions = Object.freeze({
   "app-latest-sha": "f2f855a62ee1533823fb2fe1819bc6873f108c35",
   "core-latest-tag": "v1.17.0-tm-v0.34.27",
   "core-latest-sha": "e3bad3d08ce3372fb7473b9e13be00cdaaa4fa91",
-  "node-latest-tag": "v0.7.2",
-  "node-latest-sha": "eea18a72891af318fb26abb7264416b679ec2a16",
+  "node-latest-tag": "v0.8.0-rc0",
+  "node-latest-sha": "09cf0043b199f2a329b71b3895ab32c2b42cece3",
 });
 export default blockspaceraceVersions;
