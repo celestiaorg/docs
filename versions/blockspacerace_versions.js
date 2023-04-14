c5ea08b459ddfccf3b6592a988cbd0a833a83f54
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-04-14 09:25:20 -0400

    deps: update celestia-node version

--- versions/blockspacerace_versions.js
+++ versions/blockspacerace_versions.js
@@ -3,7 +3,7 @@ const blockspaceraceVersions = Object.freeze({
   "app-latest-sha": "9698a07837c2cc1986f3f59c4cb51459da9c29e5",
   "core-latest-tag": "v1.17.0-tm-v0.34.27",
   "core-latest-sha": "e3bad3d08ce3372fb7473b9e13be00cdaaa4fa91",
-  "node-latest-tag": "v0.8.2",
-  "node-latest-sha": "8e36597a82f3b6574669d57160844078b8bdb82d",
+  "node-latest-tag": "v0.9.0-rc1",
+  "node-latest-sha": "acc89f40e1cb379ea1c2c3fe775cbbea9f0d3758",
 });
 export default blockspaceraceVersions;
