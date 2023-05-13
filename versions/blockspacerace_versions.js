e451f6f4506174c6ff7ff6745b48a9bc978d0cd9
jcstein <jcstein@users.noreply.github.com>
2023-05-13 09:11:12 +0000

    [automated GH action] update latest release tags & commit sha (blockspacerace)

--- versions/blockspacerace_versions.js
+++ versions/blockspacerace_versions.js
@@ -3,7 +3,7 @@ const blockspaceraceVersions = Object.freeze({
   "app-latest-sha": "1c51eb36f5e733e7cd0eefef67d7da9202968b87",
   "core-latest-tag": "v1.19.0-tm-v0.34.27",
   "core-latest-sha": "2a26e7a5ffb5451bf12d09fe9a88a15c6f867b62",
-  "node-latest-tag": "v0.9.3",
-  "node-latest-sha": "7f556f06e175267e0dd60b444a68554f592710a0",
+  "node-latest-tag": "v0.9.4",
+  "node-latest-sha": "876c6d04db110d52fe6ca1fd253fed5a402a8715",
 });
 export default blockspaceraceVersions;
