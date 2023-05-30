dbdff06d3bad7ffa34aab85c33f7862ebf3ae4de
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-05-30 15:43:43 -0400

    deps: bump node tag, bc v0.11.0-rc0 is not "latest"

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "ab64b67797653e99691e846a4303bd71f44d1a8e",
   "core-latest-tag": "v1.21.1-tm-v0.34.27",
   "core-latest-sha": "904ca9e002d1cd7220b4288113197e2e9bdf7ad5",
-  "node-latest-tag": "v0.10.2",
-  "node-latest-sha": "960b522ec4ed2fb2069a220e2cef2da609158f4d",
+  "node-latest-tag": "v0.11.0-rc0",
+  "node-latest-sha": "a438788f474f65aae3e33b3b8ee6db055f8d3607",
 });
 export default arabicaVersions;
