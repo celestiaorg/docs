8c224ce3fd0dc4c8dece27e13bd383f8eba80fda
jcstein <jcstein@users.noreply.github.com>
2023-05-31 16:56:12 +0000

    [automated GH action] update latest release tags & commit sha (arabica)

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "ab64b67797653e99691e846a4303bd71f44d1a8e",
   "core-latest-tag": "v1.21.1-tm-v0.34.27",
   "core-latest-sha": "904ca9e002d1cd7220b4288113197e2e9bdf7ad5",
-  "node-latest-tag": "v0.11.0-rc1",
-  "node-latest-sha": "b62452af1e7dd1cc1b9a5ca1d96d1545154b4d04",
+  "node-latest-tag": "v0.10.2",
+  "node-latest-sha": "960b522ec4ed2fb2069a220e2cef2da609158f4d",
 });
 export default arabicaVersions;
