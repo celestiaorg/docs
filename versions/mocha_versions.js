5f570a16e289722a0cdac47d68c1fad46621035d
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-06-02 11:41:28 -0400

    Update versions/mocha_versions.js

--- versions/mocha_versions.js
+++ versions/mocha_versions.js
@@ -1,5 +1,5 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v0.13.3",
+  "app-latest-tag": "v1.0.0-rc1",
   "app-latest-sha": "ab64b67797653e99691e846a4303bd71f44d1a8e",
   "core-latest-tag": "v1.21.1-tm-v0.34.27",
   "core-latest-sha": "904ca9e002d1cd7220b4288113197e2e9bdf7ad5",
