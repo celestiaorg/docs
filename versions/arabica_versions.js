dcf667477978e324770817a9b08282ca7e0fa3e3
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-06-06 11:57:16 -0400

    docs: bump back to rc0 for app for arabica (#796)

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.0.0-rc1",
-  "app-latest-sha": "049482868d2b51034e8c54415de3a2bb761b79df",
+  "app-latest-tag": "v1.0.0-rc0",
+  "app-latest-sha": "456e6fd01bb2681da6ca5c89493dd9ed73e3021f",
   "core-latest-tag": "v1.21.1-tm-v0.34.27",
   "core-latest-sha": "904ca9e002d1cd7220b4288113197e2e9bdf7ad5",
   "node-latest-tag": "v0.11.0-rc2",
