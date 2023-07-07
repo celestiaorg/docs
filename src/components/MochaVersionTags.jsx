5adf47d83380b3fd457d2849a657f2337eff8315
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-07-07 04:22:36 -0400

    docs: add Chain ID to network pages and network page (#878)

--- src/components/MochaVersionTags.jsx
+++ src/components/MochaVersionTags.jsx
@@ -5,6 +5,7 @@ import constants from "../../versions/constants.js";
 const MochaVersionTags = () => {
   return (
     <ul>
+      <li>Celestia Chain ID - <a href={`https://github.com/celestiaorg/networks/tree/master/${constants.mochaChainId}`} target="_blank" rel="noopener noreferrer">{constants.mochaChainId}</a></li>
       <li>Celestia Node - <a href={`https://github.com/celestiaorg/celestia-node/releases/tag/${mochaVersions['node-latest-tag']}`} target="_blank" rel="noopener noreferrer">{mochaVersions['node-latest-tag']}</a></li>
       <li>Celestia App - <a href={`https://github.com/celestiaorg/celestia-app/releases/tag/${mochaVersions['app-latest-tag']}`} target="_blank" rel="noopener noreferrer">{mochaVersions['app-latest-tag']}</a></li>
       <li>Rollkit - <a href={`https://github.com/rollkit/rollkit/releases/tag/${constants.mochaRollkitVersion}`} target="_blank" rel="noopener noreferrer">{constants.mochaRollkitVersion}</a></li>
