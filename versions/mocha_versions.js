81225ba0b6813a5f57bf6503002740e114f54412
mindstyle85 <55639830+mindstyle85@users.noreply.github.com>
2023-07-12 22:09:06 +0200

    Update mocha_versions.js

--- versions/mocha_versions.js
+++ versions/mocha_versions.js
@@ -1,6 +1,6 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v1.0.0-rc7",
-  "app-latest-sha": "2ad80bc4aa1be3ddae6b417189ce58088a93c666",
+  "app-latest-tag": "v1.0.0-rc9",
+  "app-latest-sha": "0a02855c534474c58267ae9d1d03ae21f7057122",
   "core-latest-tag": "v1.23.0-tm-v0.34.28",
   "core-latest-sha": "67cc27bcb051f911c40444530f4e5257b04dd469",
   "node-latest-tag": "v0.11.0-rc7",
