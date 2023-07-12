66624fa9a89edda87748186edcbf6a794ce11f68
jcstein <jcstein@users.noreply.github.com>
2023-07-12 20:28:29 +0000

    [automated GH action] update latest release tags & commit sha (mocha)

--- versions/mocha_versions.js
+++ versions/mocha_versions.js
@@ -1,8 +1,8 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v1.0.0-rc9",
-  "app-latest-sha": "0a02855c534474c58267ae9d1d03ae21f7057122",
-  "core-latest-tag": "v1.23.0-tm-v0.34.28",
-  "core-latest-sha": "67cc27bcb051f911c40444530f4e5257b04dd469",
+  "app-latest-sha": "ebfc4168cb5b5ac17a92ea0dbb9e50c78d8ae1bc",
+  "core-latest-tag": "v1.24.0-tm-v0.34.28",
+  "core-latest-sha": "d24c81afe0ecc4ccfbb7eb543a45a11794f04db4",
   "node-latest-tag": "v0.11.0-rc7",
   "node-latest-sha": "0d10cedaa0d0d117b18b618d1d3ccbe456097495",
 });
