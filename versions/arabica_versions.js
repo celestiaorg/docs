7ed072eebff696212637450bf4e8e5703c64b9cb
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-07-14 19:46:25 -0400

    Update versions/arabica_versions.js

--- versions/arabica_versions.js
+++ versions/arabica_versions.js
@@ -2,7 +2,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-tag": "v1.0.0-rc9",
   "app-latest-sha": "ebfc4168cb5b5ac17a92ea0dbb9e50c78d8ae1bc",
   "core-latest-tag": "v1.24.0-tm-v0.34.28",
-  "core-latest-sha": "d24c81afe0ecc4ccfbb7eb543a45a11794f04db4",
+  "core-latest-sha": "67cc27bcb051f911c40444530f4e5257b04dd469",
   "node-latest-tag": "v0.11.0-rc8",
   "node-latest-sha": "e41f62662cf1844ba52d7a93d03cf79b6e363897",
 });
