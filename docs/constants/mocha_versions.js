3c903f4880b62faf24a87fafebff0925a4ae0fe2
jcstein <jcstein@users.noreply.github.com>
2023-09-19 16:53:08 +0000

    [automated GH action] update latest release tags & commit sha (mocha)

--- docs/constants/mocha_versions.js
+++ docs/constants/mocha_versions.js
@@ -1,6 +1,6 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v1.0.0-rc15",
-  "app-latest-sha": "0d70807442ba0545058d353b44f6f9a583d3e11d",
+  "app-latest-tag": "v1.0.0-rc16",
+  "app-latest-sha": "44e13fad9a24f858dc2707ed3bc9f07818785206",
   "core-latest-tag": "v1.27.0-tm-v0.34.28",
   "core-latest-sha": "074d98180f2e71ea0213e4a10ad4b0c460f995da",
   "node-latest-tag": "v0.11.0-rc13",
