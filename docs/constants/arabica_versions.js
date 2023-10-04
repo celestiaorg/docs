d865e94b02c821232d9d46090abe01043e2ceffa
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-10-04 18:05:18 -0400

    💣

--- docs/constants/arabica_versions.js
+++
@@ -1,9 +0,0 @@
-const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.0.0-rc18",
-  "app-latest-sha": "6b709763dff41305da6fe3c9ec65f26691cc5163",
-  "core-latest-tag": "v1.29.0-tm-v0.34.29",
-  "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
-  "node-latest-tag": "v0.11.0-rc13",
-  "node-latest-sha": "8ca777e1a3f04b7d0494e3bae8a1c1bcf68c5d8c",
-});
-export default arabicaVersions;
