be32b6919c95911422b71ff48c6f70c70fc0e09e
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-10-04 11:38:01 -0400

    Apply suggestions from code review

--- docs/constants/mocha_versions.js
+++ docs/constants/mocha_versions.js
@@ -3,7 +3,7 @@ const mochaVersions = Object.freeze({
   "app-latest-sha": "6b709763dff41305da6fe3c9ec65f26691cc5163",
   "core-latest-tag": "v1.29.0-tm-v0.34.29",
   "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
-  "node-latest-tag": "v0.11.0-rc9.1",
-  "node-latest-sha": "8106b681f0a81b21fa0722b084a6e1e243db59be",
+  "node-latest-tag": "v0.11.0-rc13",
+  "node-latest-sha": "8ca777e1a3f04b7d0494e3bae8a1c1bcf68c5d8c",
 });
 export default mochaVersions;
