b96a4b9cd2928ac6808674f4e3ae997a0b36a49c
YazzyYaz <YazzyYaz@users.noreply.github.com>
2023-10-16 14:06:07 +0000

    [automated GH action] update latest release tags & commit sha (arabica)

--- docs/constants/arabica_versions.js
+++ docs/constants/arabica_versions.js
@@ -1,9 +1,9 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.0.0",
-  "app-latest-sha": "2335d2b7d8d9463abc85f79b430926380dd41001",
+  "app-latest-tag": "v1.1.0",
+  "app-latest-sha": "2b8cc9e23826ccb658b7dd5aa6cd51a0921a0c29",
   "core-latest-tag": "v1.29.0-tm-v0.34.29",
   "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
-  "node-latest-tag": "v0.11.0-rc15",
-  "node-latest-sha": "00b32a808c6d800e4e820ea33a6b6c2811a0ad3a",
+  "node-latest-tag": "v0.11.0",
+  "node-latest-sha": "0b854485d4ee7d84ffdaf8c2e8bd1fcedf940680",
 });
 export default arabicaVersions;
