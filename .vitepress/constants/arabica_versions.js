4db7828bb950eab0dca232a33557907b904f1c89
jcstein <jcstein@users.noreply.github.com>
2023-10-25 18:39:58 +0000

    [automated GH action] update latest release tags & commit sha (arabica)

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.1.0",
-  "app-latest-sha": "2b8cc9e23826ccb658b7dd5aa6cd51a0921a0c29",
+  "app-latest-tag": "v1.2.0",
+  "app-latest-sha": "aa54b9d05f5925adebb426abcc4c8b6dd6f534d1",
   "core-latest-tag": "v1.29.0-tm-v0.34.29",
   "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
   "node-latest-tag": "v0.11.0",
