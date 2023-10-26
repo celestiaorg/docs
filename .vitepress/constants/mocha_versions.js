0bf6c7d649c7267359df9d0659572e5955a5dd91
YazzyYaz <YazzyYaz@users.noreply.github.com>
2023-10-26 13:54:05 +0000

    [automated GH action] update latest release tags & commit sha (mocha)

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,6 +1,6 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v1.2.0",
-  "app-latest-sha": "aa54b9d05f5925adebb426abcc4c8b6dd6f534d1",
+  "app-latest-tag": "v1.3.0",
+  "app-latest-sha": "ada77509d7fdedf2a3e3400b720549365851454c",
   "core-latest-tag": "v1.29.0-tm-v0.34.29",
   "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
   "node-latest-tag": "v0.11.0",
