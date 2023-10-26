32447b9773c5e4af7cb8008fa20a87918dd74217
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-10-26 13:24:51 -0400

    fix: add workaround for non-interactive mode (curling script)
    fix: script echo
    
    fix: remove unecessary echos
    
    fix: rearrange flags, add back echo
    
    fix: add context on non-interactive mode
    
    Revert "fix: add workaround for non-interactive mode (curling script)"
    
    This reverts commit 8737c15a91037035b350a9d0b014f235cde1ef12.
    
    fix: test reply for non-interactive runs

--- public/celestia-app.sh
+++ public/celestia-app.sh
@@ -14,6 +14,7 @@ TEMP_DIR="$HOME/celestia-temp"
 
 # Check if the directory exists
 if [ -d "$TEMP_DIR" ]; then
+    REPLY=n
     read -p "Directory $TEMP_DIR exists. Do you want to clear it out? (y/n) " -n 1 -r
     echo    # move to a new line
     if [[ $REPLY =~ ^[Yy]$ ]]
@@ -133,7 +134,8 @@ rm "celestia-app_$PLATFORM.tar.gz"
 echo "Temporary files cleaned up." | tee -a "$LOGFILE"
 
 # Ask the user if they want to move the binary to /usr/local/bin
-read -p "Do you want to move the binary to /usr/local/bin? This will require sudo access. (y/n) " -n 1 -r
+REPLY=n
+read -n 1 -r -p "Do you want to move the binary to /usr/local/bin? This will require sudo access. (y/n) "
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
