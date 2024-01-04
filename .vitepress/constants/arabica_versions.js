7de165b8e9be896716bf75d4e34d8dd063e6c0f2
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2024-01-04 10:12:13 -0500

    feat: arabica-11, celestia-node v0.12.2

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "ada77509d7fdedf2a3e3400b720549365851454c",
   "core-latest-tag": "v1.29.0-tm-v0.34.29",
   "core-latest-sha": "e67de4517780e8db93c74d8eec4e9c66edffbc99",
-  "node-latest-tag": "v0.12.2-rc2",
+  "node-latest-tag": "v0.12.2",
   "node-latest-sha": "f75e255999d3e9c590b03b83f068b58cc32d2548",
 });
 export default arabicaVersions;
