3f19546a3c36991581ad814613b3d17f79e2a12b
mindstyle85 <55639830+mindstyle85@users.noreply.github.com>
2024-04-15 09:54:01 +0200

    Update mocha_versions.js

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -3,7 +3,7 @@ const mochaVersions = Object.freeze({
   "app-latest-sha": "915847191e80d836f862eea2664949d9a240abea",
   "core-latest-tag": "v1.35.0-tm-v0.34.29",
   "core-latest-sha": "f95a434e674610b3a2c5e7d26bd87da69ccaa213",
-  "node-latest-tag": "v0.13.2",
-  "node-latest-sha": "c1b41b0973e9d140b7651295e879d27ad47f42c4",
+  "node-latest-tag": "v0.13.3",
+  "node-latest-sha": "7fc5c8019271ae12acff0aec4b6c4cc1dd8b4302",
 });
 export default mochaVersions;
