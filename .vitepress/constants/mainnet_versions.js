4a0f54311e41cb3cf83ebb4013bd2ea8940a6fe9
Edgar Barrantes <edgar@barrantes.dev>
2024-04-22 19:51:48 +0200

    fix: update node version to latest on Mainnet beta (to be merged after Mocha is upgraded, and Mainnet has been upgraded) (#1523)
    * Update mainnet_versions.js
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: joshcs.eth <46639943+jcstein@users.noreply.github.com>

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -3,7 +3,7 @@ const mainnetVersions = Object.freeze({
   "app-latest-sha": "915847191e80d836f862eea2664949d9a240abea",
   "core-latest-tag": "v1.35.0-tm-v0.34.29",
   "core-latest-sha": "f95a434e674610b3a2c5e7d26bd87da69ccaa213",
-  "node-latest-tag": "v0.13.2",
-  "node-latest-sha": "c1b41b0973e9d140b7651295e879d27ad47f42c4",
+  "node-latest-tag": "v0.13.4",
+  "node-latest-sha": "13ac02fe9a659b8741c2f62fac256c4ef249b718",
 });
 export default mainnetVersions;
