5c3eeb17c617c6e2bf661bfda7feca84c5d64218
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2024-04-22 10:13:23 -0400

    Apply suggestions from code review

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -3,7 +3,7 @@ const mainnetVersions = Object.freeze({
   "app-latest-sha": "915847191e80d836f862eea2664949d9a240abea",
   "core-latest-tag": "v1.35.0-tm-v0.34.29",
   "core-latest-sha": "f95a434e674610b3a2c5e7d26bd87da69ccaa213",
-  "node-latest-tag": "v0.13.3",
-  "node-latest-sha": "05238b3e087eb9ecd3b9684cd0125f2400f6f0c7",
+  "node-latest-tag": "v0.13.4",
+  "node-latest-sha": "13ac02fe9a659b8741c2f62fac256c4ef249b718",
 });
 export default mainnetVersions;
