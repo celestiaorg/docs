aea25dc59fc2ad60b6a38253e95bb4329fda5485
smuu <smuu@users.noreply.github.com>
2024-05-03 13:27:12 +0000

    [automated GH action] update latest release tags & commit sha (arabica)

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,9 +1,9 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.8.0",
-  "app-latest-sha": "e75a1fdc8f2386d9f389cb596c88ca7cc19563af",
+  "app-latest-tag": "v1.9.0",
+  "app-latest-sha": "ad18465a88000a5b45ceb63efc7d46efaaa56f2c",
   "core-latest-tag": "v1.35.0-tm-v0.34.29",
   "core-latest-sha": "f95a434e674610b3a2c5e7d26bd87da69ccaa213",
-  "node-latest-tag": "v0.13.4",
-  "node-latest-sha": "13ac02fe9a659b8741c2f62fac256c4ef249b718",
+  "node-latest-tag": "v0.14.0-rc0",
+  "node-latest-sha": "2d2adb3a2504f42bb56e08ef2b8bacba52c54c86",
 });
 export default arabicaVersions;
