fc445f885f82cd66494beaab7dce0abc80bfc3c0
mindstyle85 <55639830+mindstyle85@users.noreply.github.com>
2024-06-17 14:04:11 +0200

    Update mainnet_versions.js

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -3,7 +3,7 @@ const mainnetVersions = Object.freeze({
   "app-latest-sha": "39cc28dd0883b339935daba972069dce168fddae",
   "core-latest-tag": "v1.36.1-tm-v0.34.29",
   "core-latest-sha": "624c43d4484a785ec855f5fb93ea571c1e728fda",
-  "node-latest-tag": "v0.13.6",
-  "node-latest-sha": "e40a10990142b443f62a1e71b7923ab993b91303",
+  "node-latest-tag": "v0.13.7",
+  "node-latest-sha": "0308aea76857ff27484946fce99004ebf10a3cb8",
 });
 export default mainnetVersions;
