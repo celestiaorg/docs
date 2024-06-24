f212ba478d9cdcc0346ff4350a19d988aaa15e69
mindstyle85 <55639830+mindstyle85@users.noreply.github.com>
2024-06-24 16:15:26 +0200

    Update arabica_versions.js (#1628)

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,9 +1,9 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v1.10.1",
-  "app-latest-sha": "39cc28dd0883b339935daba972069dce168fddae",
+  "app-latest-tag": "v1.11.0",
+  "app-latest-sha": "21b5bc747c8500e4888474df7d828e66c33f332d",
   "core-latest-tag": "v1.36.1-tm-v0.34.29",
   "core-latest-sha": "624c43d4484a785ec855f5fb93ea571c1e728fda",
-  "node-latest-tag": "v0.14.0-rc2",
-  "node-latest-sha": "fbce4b7f98c8c1c9814e72fb77a4b9aacbd2df58",
+  "node-latest-tag": "v0.14.0",
+  "node-latest-sha": "13439ccfebca67cc973cb7fa69570060e4540c13",
 });
 export default arabicaVersions;
