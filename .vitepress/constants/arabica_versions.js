09cf207132457f11bbe932fd2f29257b61f6412b
Josh Stein <46639943+jcstein@users.noreply.github.com>
2024-07-30 09:32:20 -0400

    docs: Update arabica_versions.js

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -3,7 +3,7 @@ const arabicaVersions = Object.freeze({
   "app-latest-sha": "21b5bc747c8500e4888474df7d828e66c33f332d",
   "core-latest-tag": "v1.36.1-tm-v0.34.29",
   "core-latest-sha": "624c43d4484a785ec855f5fb93ea571c1e728fda",
-  "node-latest-tag": "v0.14.0",
-  "node-latest-sha": "13439ccfebca67cc973cb7fa69570060e4540c13",
+  "node-latest-tag": "v0.14.1",
+  "node-latest-sha": "6549873516889aebcb787c4e9f2ae3b3258c386a",
 });
 export default arabicaVersions;
