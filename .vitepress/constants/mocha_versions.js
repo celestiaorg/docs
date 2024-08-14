4d09171734abeaea91dcaed135d624b8869e511d
Rootul Patel <rootulp@gmail.com>
2024-08-14 09:53:42 -0400

    fix: mocha versions for app v2.0.0

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,8 +1,8 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v1.14.0",
-  "app-latest-sha": "b6db108a444c234e4b4656031d876bc45421c5f3",
-  "core-latest-tag": "v1.39.0-tm-v0.34.29",
-  "core-latest-sha": "6333e56ff954fae056f628ad0fe459409a0b3fcd",
+  "app-latest-tag": "v2.0.0",
+  "app-latest-sha": "3554ad6608e4028e9f7d5f479a350ab12dc4e314",
+  "core-latest-tag": "v1.38.0-tm-v0.34.29",
+  "core-latest-sha": "637b18e666f995cda84ace6d7000a199b42e208b",
   "node-latest-tag": "v0.16.0-rc0",
   "node-latest-sha": "603288597fbd82c44aaa0a0ac8d187cac1860431",
 });
