1c713ebe60b8e39f082fd49eaf807740ada3c0af
rootulp <rootulp@users.noreply.github.com>
2024-08-14 13:20:57 +0000

    [automated GH action] update latest release tags & commit sha (mocha)

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,9 +1,9 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v1.11.0",
-  "app-latest-sha": "21b5bc747c8500e4888474df7d828e66c33f332d",
-  "core-latest-tag": "v1.36.1-tm-v0.34.29",
-  "core-latest-sha": "624c43d4484a785ec855f5fb93ea571c1e728fda",
-  "node-latest-tag": "v0.14.1",
-  "node-latest-sha": "6549873516889aebcb787c4e9f2ae3b3258c386a",
+  "app-latest-tag": "v1.14.0",
+  "app-latest-sha": "b6db108a444c234e4b4656031d876bc45421c5f3",
+  "core-latest-tag": "v1.39.0-tm-v0.34.29",
+  "core-latest-sha": "6333e56ff954fae056f628ad0fe459409a0b3fcd",
+  "node-latest-tag": "v0.16.0-rc0",
+  "node-latest-sha": "603288597fbd82c44aaa0a0ac8d187cac1860431",
 });
 export default mochaVersions;
