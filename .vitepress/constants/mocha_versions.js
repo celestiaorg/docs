cd25d2d3eb69878f34753b3c36b60be879271d7e
Rootul P <rootulp@gmail.com>
2024-08-27 22:09:46 -0400

    Update .vitepress/constants/mocha_versions.js

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -3,7 +3,7 @@ const mochaVersions = Object.freeze({
   "app-latest-sha": "48173df3dc78f9348eedb3796f29ef9e9e5dc584",
   "core-latest-tag": "v1.40.1-tm-v0.34.29-rc0",
   "core-latest-sha": "0d2b63836d0f4587e162bfded58f53fba238e69c",
-  "node-latest-tag": "v0.16.1-rc0",
-  "node-latest-sha": "9fd66a56132d25957fd25116ca548343fee0f3c4",
+  "node-latest-tag": "v0.16.0-rc0",
+  "node-latest-sha": "603288597fbd82c44aaa0a0ac8d187cac1860431",
 });
 export default mochaVersions;
