adbc26d4e9ec9b95b910700f0831b3c942d31c10
jcstein <46639943+jcstein@users.noreply.github.com>
2024-09-27 11:00:28 -0400

    docs: add v2.2.0-arabica

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v2.1.2",
-  "app-latest-sha": "48173df3dc78f9348eedb3796f29ef9e9e5dc584",
+  "app-latest-tag": "v2.2.0-arabica",
+  "app-latest-sha": "d3f8b2997fcbc8e7f43785a9513b2d0c91675087",
   "core-latest-tag": "v1.40.1-tm-v0.34.29-rc0",
   "core-latest-sha": "0d2b63836d0f4587e162bfded58f53fba238e69c",
   "node-latest-tag": "v0.18.1-arabica",
