8a14ecb4e1fd771b0b28c3ea2ac6f74428789393
jcstein <jcstein@users.noreply.github.com>
2024-12-13 18:01:23 +0000

    [automated GH action] update latest release tags & commit sha (mocha)

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -3,7 +3,7 @@ const mochaVersions = Object.freeze({
   "app-latest-sha": "b24feac743182b33192d46ed42d8f89578c084ab",
   "core-latest-tag": "v1.44.0-tm-v0.34.35",
   "core-latest-sha": "a1268f7ae3e688144a613c8a439dd31818aae07d",
-  "node-latest-tag": "v0.20.4-mocha",
+  "node-latest-tag": "v0.20.4",
   "node-latest-sha": "51b79431533576a5cddf1235bdf3751e4c014212",
 });
 export default mochaVersions;
