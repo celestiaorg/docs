179cd82ab516f2ca861e9a35b204c67803ca8abb
aWN4Y25pa2EK <19519604+aWN4Y25pa2EK@users.noreply.github.com>
2024-12-13 18:03:57 +0000

    fix(mocha_versions.js): update app-latest-tag version to include '-mocha' suffix for clarity in versioning

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,5 +1,5 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v3.0.2",
+  "app-latest-tag": "v3.0.2-mocha",
   "app-latest-sha": "b24feac743182b33192d46ed42d8f89578c084ab",
   "core-latest-tag": "v1.44.0-tm-v0.34.35",
   "core-latest-sha": "a1268f7ae3e688144a613c8a439dd31818aae07d",
