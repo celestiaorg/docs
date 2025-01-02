b8cf1a76c55bd3efc5dc5aed89d88bf3f33120df
jcstein <joshcs@celestia.org>
2025-01-02 14:34:25 +0000

    deploy: 964ed094efb05dc35e275b8264e00e5f3ddfdbb6

---
+++ assets/how-to-guides_validator-node.md.7f709a18.lean.js
@@ -0,0 +1,37 @@
+import{c as o}from"./chunks/constants.b02263eb.js";import{o as l,c as p,k as s,a,t as n,l as t,Q as e}from"./chunks/framework.1a91c06a.js";const c="/img/nodes/validator.png",r=e("",19),i={class:"language-bash vp-adaptive-theme"},d=s("button",{title:"Copy Code",class:"copy"},null,-1),h=s("span",{class:"lang"},"bash",-1),u={class:"shiki github-dark vp-code-dark"},_=e("",1),y=e("",1),g={class:"line"},E=s("span",{style:{color:"#E1E4E8"}},"--from=$VALIDATOR_WALLET ",-1),C={style:{color:"#79B8FF"}},m=s("span",{style:{color:"#E1E4E8"}}," ",-1),F=s("span",{style:{color:"#79B8FF"}},"\\",-1),v=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"--fees=21000utia")],-1),b={class:"shiki github-light vp-code-light"},T=e("",1),k=e("",1),f={class:"line"},A=s("span",{style:{color:"#24292E"}},"--from=$VALIDATOR_WALLET ",-1),S={style:{color:"#005CC5"}},B=s("span",{style:{color:"#24292E"}}," ",-1),x=s("span",{style:{color:"#005CC5"}},"\\",-1),q=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"--fees=21000utia")],-1),w=e("",30),I={class:"language-bash vp-adaptive-theme"},P=s("button",{title:"Copy Code",class:"copy"},null,-1),V=s("span",{class:"lang"},"bash",-1),R={class:"shiki github-dark vp-code-dark"},D=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"MONIKER"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"your_moniker"')],-1),L=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#F97583"}},"="),s("span",{style:{color:"#9ECBFF"}},'"validator"')],-1),N=s("span",{class:"line"},null,-1),O=e("",1),M=e("",1),z=e("",1),U=e("",1),W={class:"line"},$=s("span",{style:{color:"#E1E4E8"}},"    ",-1),H={style:{color:"#79B8FF"}},Y=s("span",{style:{color:"#E1E4E8"}}," ",-1),J=s("span",{style:{color:"#79B8FF"}},"\\",-1),G=e("",1),K=e("",1),Q=e("",1),X=e("",1),j=e("",1),Z=e("",1),ss=e("",1),as=s("span",{class:"line"},[s("span",{style:{color:"#E1E4E8"}},"    "),s("span",{style:{color:"#79B8FF"}},"--gas=220000")],-1),es={class:"shiki github-light vp-code-light"},os=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"MONIKER"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"your_moniker"')],-1),ns=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"VALIDATOR_WALLET"),s("span",{style:{color:"#D73A49"}},"="),s("span",{style:{color:"#032F62"}},'"validator"')],-1),ts=s("span",{class:"line"},null,-1),ls=e("",1),ps=e("",1),cs=e("",1),rs=e("",1),is={class:"line"},ds=s("span",{style:{color:"#24292E"}},"    ",-1),hs={style:{color:"#005CC5"}},us=s("span",{style:{color:"#24292E"}}," ",-1),_s=s("span",{style:{color:"#005CC5"}},"\\",-1),ys=e("",1),gs=e("",1),Es=e("",1),Cs=e("",1),ms=e("",1),Fs=e("",1),vs=e("",1),bs=s("span",{class:"line"},[s("span",{style:{color:"#24292E"}},"    "),s("span",{style:{color:"#005CC5"}},"--gas=220000")],-1),Ts=e("",17),qs=JSON.parse('{"title":"Setting up a Celestia validator node","description":"Learn how to set up a Celestia validator node.","frontmatter":{"description":"Learn how to set up a Celestia validator node.","outline":"deep","head":[["meta",{"property":"og:title","content":"Setting up a Celestia validator node | Celestia Docs"}],["meta",{"property":"og:description","content":"Learn how to set up a Celestia validator node."}]]},"headers":[],"relativePath":"how-to-guides/validator-node.md","filePath":"how-to-guides/validator-node.md","lastUpdated":1729099273000}'),ks={name:"how-to-guides/validator-node.md"},ws=Object.assign(ks,{setup(fs){return(As,Ss)=>(l(),p("div",null,[r,s("div",i,[d,h,s("pre",u,[s("code",null,[_,a(`
+`),y,a(`
+`),s("span",g,[E,s("span",C,"--chain-id="+n(t(o).mochaChainId),1),m,F]),a(`
+`),v])]),s("pre",b,[s("code",null,[T,a(`
+`),k,a(`
+`),s("span",f,[A,s("span",S,"--chain-id="+n(t(o).mochaChainId),1),B,x]),a(`
+`),q])])]),w,s("div",I,[P,V,s("pre",R,[s("code",null,[D,a(`
+`),L,a(`
+`),N,a(`
+`),O,a(`
+`),M,a(`
+`),z,a(`
+`),U,a(`
+`),s("span",W,[$,s("span",H,"--chain-id="+n(t(o).mochaChainId),1),Y,J]),a(`
+`),G,a(`
+`),K,a(`
+`),Q,a(`
+`),X,a(`
+`),j,a(`
+`),Z,a(`
+`),ss,a(`
+`),as])]),s("pre",es,[s("code",null,[os,a(`
+`),ns,a(`
+`),ts,a(`
+`),ls,a(`
+`),ps,a(`
+`),cs,a(`
+`),rs,a(`
+`),s("span",is,[ds,s("span",hs,"--chain-id="+n(t(o).mochaChainId),1),us,_s]),a(`
+`),ys,a(`
+`),gs,a(`
+`),Es,a(`
+`),Cs,a(`
+`),ms,a(`
+`),Fs,a(`
+`),vs,a(`
+`),bs])])]),Ts]))}});export{qs as __pageData,ws as default};
---
+++ assets/tutorials_wallets.md.97460096.lean.js
@@ -0,0 +1,55 @@
+import{c as p}from"./chunks/constants.b02263eb.js";import{_ as e,o as t,c,k as s,a as n,t as l,Q as o}from"./chunks/framework.1a91c06a.js";const r={data(){return{constants:p}}},hn=JSON.parse('{"title":"Wallet integrations with Celestia","description":"How you can add Celestia network parameters to wallets such as Keplr, Leap, and Cosmostation.","frontmatter":{"description":"How you can add Celestia network parameters to wallets such as Keplr, Leap, and Cosmostation.","head":[["meta",{"property":"og:title","content":"Wallet integrations with Celestia | Celestia Docs"}],["meta",{"property":"og:description","content":"How you can add Celestia network parameters to wallets such as Keplr, Leap, and Cosmostation."}]]},"headers":[],"relativePath":"tutorials/wallets.md","filePath":"tutorials/wallets.md","lastUpdated":1729099273000}'),i=o("",7),E={class:"vp-code-group vp-adaptive-theme"},y=o("",1),d={class:"blocks"},_={class:"language-js vp-adaptive-theme active"},h=s("button",{title:"Copy Code",class:"copy"},null,-1),u=s("span",{class:"lang"},"js",-1),F={class:"shiki github-dark vp-code-dark"},A=s("span",{class:"line"},[s("span",{style:{color:"#F97583"}},"import"),s("span",{style:{color:"#E1E4E8"}}," "),s("span",{style:{color:"#9ECBFF"}},"'@site/src/components/AddNetworkKeplr'")],-1),C=s("span",{class:"line"},null,-1),m=o("",1),w={class:"line"},T={style:{color:"#9ECBFF"}},b=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  chainName: 'Celestia',")],-1),q={class:"line"},g={style:{color:"#9ECBFF"}},k={class:"line"},D={style:{color:"#9ECBFF"}},B=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"}`"),s("span",{style:{color:"#E1E4E8"}},"}")],-1),I=s("span",{class:"line"},null,-1),S=o("",1),P={class:"shiki github-light vp-code-light"},N=s("span",{class:"line"},[s("span",{style:{color:"#D73A49"}},"import"),s("span",{style:{color:"#24292E"}}," "),s("span",{style:{color:"#032F62"}},"'@site/src/components/AddNetworkKeplr'")],-1),v=s("span",{class:"line"},null,-1),f=o("",1),R={class:"line"},x={style:{color:"#032F62"}},V=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  chainName: 'Celestia',")],-1),M={class:"line"},K={style:{color:"#032F62"}},U={class:"line"},L={style:{color:"#032F62"}},G=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"}`"),s("span",{style:{color:"#24292E"}},"}")],-1),j=s("span",{class:"line"},null,-1),O=o("",1),Y={class:"language-js vp-adaptive-theme"},H=s("button",{title:"Copy Code",class:"copy"},null,-1),W=s("span",{class:"lang"},"js",-1),J={class:"shiki github-dark vp-code-dark"},Q=s("span",{class:"line"},[s("span",{style:{color:"#F97583"}},"import"),s("span",{style:{color:"#E1E4E8"}}," "),s("span",{style:{color:"#9ECBFF"}},"'@site/src/components/AddNetworkKeplr'")],-1),z=s("span",{class:"line"},null,-1),X=o("",1),Z={class:"line"},$={style:{color:"#9ECBFF"}},ss=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  chainName: 'Mocha testnet',")],-1),ns={class:"line"},as={style:{color:"#9ECBFF"}},ls={class:"line"},os={style:{color:"#9ECBFF"}},ps=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"}`"),s("span",{style:{color:"#E1E4E8"}},"}")],-1),es=s("span",{class:"line"},null,-1),ts=o("",1),cs={class:"shiki github-light vp-code-light"},rs=s("span",{class:"line"},[s("span",{style:{color:"#D73A49"}},"import"),s("span",{style:{color:"#24292E"}}," "),s("span",{style:{color:"#032F62"}},"'@site/src/components/AddNetworkKeplr'")],-1),is=s("span",{class:"line"},null,-1),Es=o("",1),ys={class:"line"},ds={style:{color:"#032F62"}},_s=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  chainName: 'Mocha testnet',")],-1),hs={class:"line"},us={style:{color:"#032F62"}},Fs={class:"line"},As={style:{color:"#032F62"}},Cs=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"}`"),s("span",{style:{color:"#24292E"}},"}")],-1),ms=s("span",{class:"line"},null,-1),ws=o("",1),Ts={class:"language-js vp-adaptive-theme"},bs=s("button",{title:"Copy Code",class:"copy"},null,-1),qs=s("span",{class:"lang"},"js",-1),gs={class:"shiki github-dark vp-code-dark"},ks=s("span",{class:"line"},[s("span",{style:{color:"#F97583"}},"import"),s("span",{style:{color:"#E1E4E8"}}," "),s("span",{style:{color:"#9ECBFF"}},"'@site/src/components/AddNetworkKeplr'")],-1),Ds=s("span",{class:"line"},null,-1),Bs=o("",1),Is={class:"line"},Ss={style:{color:"#9ECBFF"}},Ps=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"  chainName: 'Arabica devnet',")],-1),Ns={class:"line"},vs={style:{color:"#9ECBFF"}},fs={class:"line"},Rs={style:{color:"#9ECBFF"}},xs=s("span",{class:"line"},[s("span",{style:{color:"#9ECBFF"}},"}`"),s("span",{style:{color:"#E1E4E8"}},"}")],-1),Vs=s("span",{class:"line"},null,-1),Ms=o("",1),Ks={class:"shiki github-light vp-code-light"},Us=s("span",{class:"line"},[s("span",{style:{color:"#D73A49"}},"import"),s("span",{style:{color:"#24292E"}}," "),s("span",{style:{color:"#032F62"}},"'@site/src/components/AddNetworkKeplr'")],-1),Ls=s("span",{class:"line"},null,-1),Gs=o("",1),js={class:"line"},Os={style:{color:"#032F62"}},Ys=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"  chainName: 'Arabica devnet',")],-1),Hs={class:"line"},Ws={style:{color:"#032F62"}},Js={class:"line"},Qs={style:{color:"#032F62"}},zs=s("span",{class:"line"},[s("span",{style:{color:"#032F62"}},"}`"),s("span",{style:{color:"#24292E"}},"}")],-1),Xs=s("span",{class:"line"},null,-1),Zs=o("",1),$s=s("p",null,"Now, we can connect to the network that you would like to use in Keplr wallet.",-1),sn=s("h2",{id:"adding-a-custom-chain-to-leap",tabindex:"-1"},[n("Adding a custom chain to Leap "),s("a",{class:"header-anchor",href:"#adding-a-custom-chain-to-leap","aria-label":'Permalink to "Adding a custom chain to Leap"'},"​")],-1),nn=s("p",null,"If you want to add a custom chain to Leap, you can do so by:",-1),an=s("ol",null,[s("li",null,"Clicking the Cosmos logo in the top corner of Leap wallet"),s("li",null,'Scrolling down and clicking "Add new chain"')],-1),ln=s("p",null,"You can then add the following parameters:",-1),on=o("",8),pn=o("",8);function en(tn,cn,rn,En,a,yn){return t(),c("div",null,[i,s("div",E,[y,s("div",d,[s("div",_,[h,u,s("pre",F,[s("code",null,[A,n(`
+`),C,n(`
+`),m,n(`
+`),s("span",w,[s("span",T,"  chainId: '"+l(a.constants.mainnetChainId)+"',",1)]),n(`
+`),b,n(`
+`),s("span",q,[s("span",g,"  rpc: '"+l(a.constants.mainnetRpcUrl)+"',",1)]),n(`
+`),s("span",k,[s("span",D,"  rest: '"+l(a.constants.mainnetRestUrl)+"'",1)]),n(`
+`),B,n(`
+`),I,n(`
+`),S])]),s("pre",P,[s("code",null,[N,n(`
+`),v,n(`
+`),f,n(`
+`),s("span",R,[s("span",x,"  chainId: '"+l(a.constants.mainnetChainId)+"',",1)]),n(`
+`),V,n(`
+`),s("span",M,[s("span",K,"  rpc: '"+l(a.constants.mainnetRpcUrl)+"',",1)]),n(`
+`),s("span",U,[s("span",L,"  rest: '"+l(a.constants.mainnetRestUrl)+"'",1)]),n(`
+`),G,n(`
+`),j,n(`
+`),O])])]),s("div",Y,[H,W,s("pre",J,[s("code",null,[Q,n(`
+`),z,n(`
+`),X,n(`
+`),s("span",Z,[s("span",$,"  chainId: '"+l(a.constants.mochaChainId)+"',",1)]),n(`
+`),ss,n(`
+`),s("span",ns,[s("span",as,"  rpc: '"+l(a.constants.mochaRpcUrl)+"',",1)]),n(`
+`),s("span",ls,[s("span",os,"  rest: '"+l(a.constants.mochaRestUrl)+"'",1)]),n(`
+`),ps,n(`
+`),es,n(`
+`),ts])]),s("pre",cs,[s("code",null,[rs,n(`
+`),is,n(`
+`),Es,n(`
+`),s("span",ys,[s("span",ds,"  chainId: '"+l(a.constants.mochaChainId)+"',",1)]),n(`
+`),_s,n(`
+`),s("span",hs,[s("span",us,"  rpc: '"+l(a.constants.mochaRpcUrl)+"',",1)]),n(`
+`),s("span",Fs,[s("span",As,"  rest: '"+l(a.constants.mochaRestUrl)+"'",1)]),n(`
+`),Cs,n(`
+`),ms,n(`
+`),ws])])]),s("div",Ts,[bs,qs,s("pre",gs,[s("code",null,[ks,n(`
+`),Ds,n(`
+`),Bs,n(`
+`),s("span",Is,[s("span",Ss,"  chainId: '"+l(a.constants.arabicaChainId)+"',",1)]),n(`
+`),Ps,n(`
+`),s("span",Ns,[s("span",vs,"  rpc: '"+l(a.constants.arabicaRpcUrl)+"',",1)]),n(`
+`),s("span",fs,[s("span",Rs,"  rest: '"+l(a.constants.arabicaRestUrl)+"'",1)]),n(`
+`),xs,n(`
+`),Vs,n(`
+`),Ms])]),s("pre",Ks,[s("code",null,[Us,n(`
+`),Ls,n(`
+`),Gs,n(`
+`),s("span",js,[s("span",Os,"  chainId: '"+l(a.constants.arabicaChainId)+"',",1)]),n(`
+`),Ys,n(`
+`),s("span",Hs,[s("span",Ws,"  rpc: '"+l(a.constants.arabicaRpcUrl)+"',",1)]),n(`
+`),s("span",Js,[s("span",Qs,"  rest: '"+l(a.constants.arabicaRestUrl)+"'",1)]),n(`
+`),zs,n(`
+`),Xs,n(`
+`),Zs])])])])]),$s,sn,nn,an,ln,s("ul",null,[s("li",null,[n("Chain Id: "),s("code",null,l(a.constants.arabicaChainId),1)]),on]),pn])}const un=e(r,[["render",en]]);export{hn as __pageData,un as default};
