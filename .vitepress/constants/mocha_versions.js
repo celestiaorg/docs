87d2e2c445591b5f12cc735f44aad04e269318c9
jcstein <46639943+jcstein@users.noreply.github.com>
2025-03-04 14:41:40 -0500

    docs: update to new vers

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,5 +1,5 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v3.4.0-rc0",
+  "app-latest-tag": "v3.4.0-mocha",
   "app-latest-sha": "f8c4c7439320925068e8a7580110dbf28f7ba295",
   "core-latest-tag": "v1.49.0-tm-v0.34.35",
   "core-latest-sha": "511badf2d82480b5297a49469583fe176610941a",
