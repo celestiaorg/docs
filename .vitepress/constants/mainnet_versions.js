e4fb5274edd645c005349a4d7b80230d49e3a600
jcstein <46639943+jcstein@users.noreply.github.com>
2025-03-19 11:40:31 -0400

    docs: add mainnet 21.9

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -3,7 +3,7 @@ const mainnetVersions = Object.freeze({
   "app-latest-sha": "f8c4c7439320925068e8a7580110dbf28f7ba295",
   "core-latest-tag": "v1.49.0-tm-v0.34.35",
   "core-latest-sha": "511badf2d82480b5297a49469583fe176610941a",
-  "node-latest-tag": "v0.21.5",
-  "node-latest-sha": "d57107415355ef6fee3fc01ba12cd3e48d7a310c",
+  "node-latest-tag": "v0.21.9",
+  "node-latest-sha": "3d8fb74bc073fb23303dcfc2804ab85ce316fa24",
 });
 export default mainnetVersions;
