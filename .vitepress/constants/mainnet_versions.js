8173dea0e9e260191fd4c542bbf759fc6e40a0b7
jcstein <jcstein@users.noreply.github.com>
2025-03-26 16:34:53 +0000

    [automated GH action] update latest release tags & commit sha (mainnet)

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -1,9 +1,9 @@
 const mainnetVersions = Object.freeze({
-  "app-latest-tag": "v3.4.0",
-  "app-latest-sha": "f8c4c7439320925068e8a7580110dbf28f7ba295",
-  "core-latest-tag": "v1.49.0-tm-v0.34.35",
-  "core-latest-sha": "511badf2d82480b5297a49469583fe176610941a",
-  "node-latest-tag": "v0.21.9",
-  "node-latest-sha": "3d8fb74bc073fb23303dcfc2804ab85ce316fa24",
+  "app-latest-tag": "v3.5.1-arabica",
+  "app-latest-sha": "4c2dcea9d4d9f797b3c78c7c5772f86ea4535f41",
+  "core-latest-tag": "v1.51.0-tm-v0.34.35",
+  "core-latest-sha": "89270d0916f991edecd34d258ba24088c1756d4b",
+  "node-latest-tag": "v0.22.0-mocha",
+  "node-latest-sha": "79e36c067407af4be803b03a7b0321cfda090086",
 });
 export default mainnetVersions;
