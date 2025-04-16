5c6476a72032de50d70d7dfa96e3cf52e4baa6fb
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-04-16 11:54:23 -0400

    Apply suggestions from code review

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -3,7 +3,7 @@ const mochaVersions = Object.freeze({
   "app-latest-sha": "a91f373fba51476c72982618108c7b8a1126c757",
   "core-latest-tag": "v1.53.0-tm-v0.38.17-rc2",
   "core-latest-sha": "092d3ad8506c67c3d138c06206cd2c41586cc1be",
-  "node-latest-tag": "v0.21.9-mammoth-v0.0.16",
-  "node-latest-sha": "f4c673bae33309cf73ff2b17f329c89841ddfd03",
+  "node-latest-tag": "v0.22.0-mocha",
+  "node-latest-sha": "79e36c067407af4be803b03a7b0321cfda090086",
 });
 export default mochaVersions;
