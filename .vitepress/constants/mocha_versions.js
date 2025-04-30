0806014fed57d692b8f2f458c2e516d6a127b767
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-04-30 11:36:15 -0400

    Apply suggestions from code review

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,9 +1,9 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "multiplexer/v0.3.0",
-  "app-latest-sha": "3d4f2d074c147e24598d85885bed44bbc8377dfb",
+  "app-latest-tag": "v3.8.1-mocha",
+  "app-latest-sha": "a91f373fba51476c72982618108c7b8a1126c757",
   "core-latest-tag": "v1.53.0-tm-v0.38.17",
   "core-latest-sha": "4acfd99c3893123775c533465f164f771d3a6f15",
-  "node-latest-tag": "v0.23.0-rc0",
-  "node-latest-sha": "38453aef06b0b6445c711591fe5dc654a0978396",
+  "node-latest-tag": "v0.22.2-mocha",
+  "node-latest-sha": "5dc98d8b249bdb9f9c56d255ce233ecb1c713cfd",
 });
 export default mochaVersions;
