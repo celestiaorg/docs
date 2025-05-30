92bf2f712f41bc0c619bf4772108dbf40578a7ff
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-05-30 10:09:23 -0400

    Apply suggestions from code review

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,9 +1,9 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v4.0.2-rc2",
+  "app-latest-tag": "v4.0.2-arabica",
   "app-latest-sha": "4fc26c1e3f1aa590da31f8f8c8400357807f6556",
   "core-latest-tag": "v1.54.0-tm-v0.38.17",
   "core-latest-sha": "ba4d47684ed5acfba0cd3c7a153cd6ad170fbe02",
-  "node-latest-tag": "v0.23.0-mocha",
-  "node-latest-sha": "a0c0547dcb670558bf81918bf06830acd26d3a8c",
+  "node-latest-tag": "v0.23.0-arabica",
+  "node-latest-sha": "b814f44afb5bde01b3714c4f21fe34203d90405d",
 });
 export default arabicaVersions;
