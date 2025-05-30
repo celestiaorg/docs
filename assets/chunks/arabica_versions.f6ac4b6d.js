3c6b78bd4ff54fc756a53b29e39231df590ff770
jcstein <joshcs@celestia.org>
2025-05-30 14:13:09 +0000

    deploy: 916e0221c5fae5ecbd47c031ae76b492cd95ddf7

---
+++ assets/chunks/arabica_versions.f6ac4b6d.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v4.0.2-arabica","app-latest-sha":"4fc26c1e3f1aa590da31f8f8c8400357807f6556","core-latest-tag":"v1.54.0-tm-v0.38.17","core-latest-sha":"ba4d47684ed5acfba0cd3c7a153cd6ad170fbe02","node-latest-tag":"v0.23.0-arabica","node-latest-sha":"b814f44afb5bde01b3714c4f21fe34203d90405d"});export{a};
---
+++ assets/chunks/mainnet_versions.d36c8623.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v3.8.1","app-latest-sha":"a91f373fba51476c72982618108c7b8a1126c757","core-latest-tag":"v1.53.0-tm-v0.38.17","core-latest-sha":"4acfd99c3893123775c533465f164f771d3a6f15","node-latest-tag":"v0.22.2","node-latest-sha":"5dc98d8b249bdb9f9c56d255ce233ecb1c713cfd"});export{a as m};
