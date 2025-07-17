2402f2831d7d16f7dc70ca68bfcfe67aab44f024
jcstein <46639943+jcstein@users.noreply.github.com>
2025-07-17 10:52:38 -0400

    docs: add new node ver and clean up constants

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
   "app-latest-tag": "v4.0.10-arabica",
   "app-latest-sha": "117b77cc511850eb24ac02472f0b18aec99a76cd",
-  "node-latest-tag": "v0.23.4-arabica",
-  "node-latest-sha": "7854bc92e8e61b9745d6d8a1ba7a0c4dc27c6c9b",
+  "node-latest-tag": "v0.23.5-arabica",
+  "node-latest-sha": "3164fac10176116599d9df8eba00797a66f2b317",
 });
 export default arabicaVersions;
--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,7 +1,7 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v4.0.10-mocha",
   "app-latest-sha": "117b77cc511850eb24ac02472f0b18aec99a76cd",
-  "node-latest-tag": "v0.23.4-mocha",
-  "node-latest-sha": "2a729736c082b971ad6aa1386ef5b2df39eb0565",
+  "node-latest-tag": "v0.23.5-mocha",
+  "node-latest-sha": "3164fac10176116599d9df8eba00797a66f2b317",
 });
 export default mochaVersions;
