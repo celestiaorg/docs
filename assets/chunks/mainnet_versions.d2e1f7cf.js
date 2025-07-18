3c3a5b415485bd39bb0b09de08bc1daebb6a5d1c
jcstein <joshcs@celestia.org>
2025-07-18 22:28:02 +0000

    deploy: 5d1479d7eaf7b8c00bf1f2a4889ebfb8e9859c8b

---
+++ assets/chunks/mainnet_versions.d2e1f7cf.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v4.0.10","app-latest-sha":"117b77cc511850eb24ac02472f0b18aec99a76cd","node-latest-tag":"v0.23.5","node-latest-sha":"1a02cee72b85ce0afa6d96c8018782bbaab79a3d"});export{a as m};
