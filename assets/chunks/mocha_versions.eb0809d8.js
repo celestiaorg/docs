85922896d9f838f84df4cb3737ccadffcb385168
jcstein <joshcs@celestia.org>
2025-07-24 17:43:19 +0000

    deploy: ec5872417cc592e738ed117eef607e0c9a18762f

---
+++ assets/chunks/mocha_versions.eb0809d8.js
@@ -0,0 +1,1 @@
+const a=Object.freeze({"app-latest-tag":"v4.0.10-mocha","app-latest-sha":"117b77cc511850eb24ac02472f0b18aec99a76cd","node-latest-tag":"v0.23.5-mocha","node-latest-sha":"3164fac10176116599d9df8eba00797a66f2b317"});export{a as m};
