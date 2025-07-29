46a96ecbf685bca9e2305881ed2ad560397862eb
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-07-29 12:14:15 -0400

    docs: update arabica_versions.js (#2178)
    * docs: update arabica_versions.js
    
    https://github.com/celestiaorg/celestia-app/releases/tag/v5.0.0-arabica
    
    * Apply suggestions from code review

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v4.0.10-arabica",
-  "app-latest-sha": "117b77cc511850eb24ac02472f0b18aec99a76cd",
-  "node-latest-tag": "v0.23.5-arabica",
-  "node-latest-sha": "3164fac10176116599d9df8eba00797a66f2b317",
+  "app-latest-tag": "v5.0.0-arabica",
+  "app-latest-sha": "f29466ce222cbacaed928f09184900721b14443b",
+  "node-latest-tag": "v0.24.1-arabica",
+  "node-latest-sha": "a3d8b8d7e0e5042e32cb87ebaa3f678a455e3cf1",
 });
 export default arabicaVersions;
