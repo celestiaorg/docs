837ab16e480da1e3c1eeb365ff64b18531a008c0
Nhu Viet Nguyen <braveryandglory@gmail.com>
2025-07-30 14:33:03 +0200

    mocha: update node version to reflect v5 changes

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -2,6 +2,6 @@ const mochaVersions = Object.freeze({
   "app-latest-tag": "v5.0.1-mocha",
   "app-latest-sha": "fd9f2ebe58abdbc0b2d6e45ecdd66c4184f4a74c",
   "node-latest-tag": "v0.24.1-mocha",
-  "node-latest-sha": "a3d8b8d7e0e5042e32cb87ebaa3f678a455e3cf1",
+  "node-latest-sha": "ad99ae625e9f01b8a80df0a173f2df557d0c510a",
 });
 export default mochaVersions;
