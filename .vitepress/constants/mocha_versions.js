ae1fedf412c88a183014e2b234024b0a6b07567b
jcstein <46639943+jcstein@users.noreply.github.com>
2025-08-15 17:39:46 +0200

    docs: v5.0.2-mocha

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,6 +1,6 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v5.0.1-mocha",
-  "app-latest-sha": "fd9f2ebe58abdbc0b2d6e45ecdd66c4184f4a74c",
+  "app-latest-tag": "v5.0.2-mocha",
+  "app-latest-sha": "323dda499ade71437a9855d99ca034d4e4869d66",
   "node-latest-tag": "v0.24.1-mocha",
   "node-latest-sha": "ad99ae625e9f01b8a80df0a173f2df557d0c510a",
 });
