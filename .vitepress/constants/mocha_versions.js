2ef8cf196730c4ba1b99750a64956cc3ed2109a5
jcstein <46639943+jcstein@users.noreply.github.com>
2025-08-18 17:57:12 +0200

    chore: mocha v0.25.2-mocha

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,7 +1,7 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v5.0.2-mocha",
   "app-latest-sha": "323dda499ade71437a9855d99ca034d4e4869d66",
-  "node-latest-tag": "v0.24.1-mocha",
-  "node-latest-sha": "ad99ae625e9f01b8a80df0a173f2df557d0c510a",
+  "node-latest-tag": "v0.25.2-mocha",
+  "node-latest-sha": "37c99f21098b785e174c44f0ef3e2b0dbd0049d5",
 });
 export default mochaVersions;
