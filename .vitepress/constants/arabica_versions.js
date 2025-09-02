ca106a328012d8dec1013b55b332bb1227de9025
Gabriel Barros <gabrielb@celestia.org>
2025-09-02 11:47:34 -0300

    Update node versions to v0.25.3 in arabica, mainnet, and mocha constants

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
   "app-latest-tag": "v5.0.3-arabica",
   "app-latest-sha": "85b61e09b8bacb778d190113621a67feaab8c4c4",
-  "node-latest-tag": "v0.25.2-arabica",
-  "node-latest-sha": "37c99f21098b785e174c44f0ef3e2b0dbd0049d5",
+  "node-latest-tag": "v0.25.3-arabica",
+  "node-latest-sha": "f28297bad87d1116973807a5f5c0f002fd608321",
 });
 export default arabicaVersions;
--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -1,7 +1,7 @@
 const mainnetVersions = Object.freeze({
   "app-latest-tag": "v5.0.2",
   "app-latest-sha": "323dda499ade71437a9855d99ca034d4e4869d66",
-  "node-latest-tag": "v0.24.1",
-  "node-latest-sha": "fb95d459d5c7ff14ebf697792287ca1cfd0848fa",
+  "node-latest-tag": "v0.25.3",
+  "node-latest-sha": "8bee349fb74ebb8fdcbe68fad4c9dae750a91807",
 });
 export default mainnetVersions;
