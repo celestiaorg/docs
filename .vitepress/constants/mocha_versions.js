ded0ecc0cf10719f86b6910e8647677e5eddf815
Gabriel Barros <gabrielb@celestia.org>
2025-09-02 11:55:26 -0300

    Update node versions to v0.25.3 in arabica, mainnet, and mocha constants (#2221)
    Co-authored-by: Josh Stein <46639943+jcstein@users.noreply.github.com>

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,7 +1,7 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v5.0.2-mocha",
   "app-latest-sha": "323dda499ade71437a9855d99ca034d4e4869d66",
-  "node-latest-tag": "v0.25.2-mocha",
-  "node-latest-sha": "37c99f21098b785e174c44f0ef3e2b0dbd0049d5",
+  "node-latest-tag": "v0.25.3-mocha",
+  "node-latest-sha": "f28297bad87d1116973807a5f5c0f002fd608321",
 });
 export default mochaVersions;
