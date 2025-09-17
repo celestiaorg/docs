61f1d43503636aa254e8fe84740c5b0bbbee926a
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-09-17 11:53:57 -0600

    chore: v0.26.1-mocha and v5.0.5 mainnet (#2237)

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,7 +1,7 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v5.0.5-mocha",
   "app-latest-sha": "bb1b85dea2aba0228b4502f02f0c7ee9d7014ab7",
-  "node-latest-tag": "v0.25.3-mocha",
-  "node-latest-sha": "f28297bad87d1116973807a5f5c0f002fd608321",
+  "node-latest-tag": "v0.26.1-mocha",
+  "node-latest-sha": "5782ccec50f24ae8ca0ea7b97d887d086e72ac84",
 });
 export default mochaVersions;
