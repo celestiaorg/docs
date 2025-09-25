f7d0f316833743aa7093aafd21b467e1259b4987
jcstein <46639943+jcstein@users.noreply.github.com>
2025-09-25 13:31:26 -0600

    docs: add 27.3-network versions

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
   "app-latest-tag": "v6.0.5-arabica",
   "app-latest-sha": "6d05e9ea9ad801cb717df404bc50f65583cc2033",
-  "node-latest-tag": "v0.27.2-arabica",
-  "node-latest-sha": "6731b09b5f759120a0cba6265aea28094f2e2c16",
+  "node-latest-tag": "v0.27.3-arabica",
+  "node-latest-sha": "7a7b3bf57b4e1f44fc5aaead646b8014c610cbbf",
 });
 export default arabicaVersions;
--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,7 +1,7 @@
 const mochaVersions = Object.freeze({
   "app-latest-tag": "v6.0.5-mocha",
   "app-latest-sha": "6d05e9ea9ad801cb717df404bc50f65583cc2033",
-  "node-latest-tag": "v0.27.2-mocha",
-  "node-latest-sha": "d2040d923130194fa53dafc1016e2fc2b4d9e4d7",
+  "node-latest-tag": "v0.27.3-mocha",
+  "node-latest-sha": "7a7b3bf57b4e1f44fc5aaead646b8014c610cbbf",
 });
 export default mochaVersions;
