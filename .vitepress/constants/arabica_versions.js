57f5e2b5e022339b02ed4fce8e1a98b1bfcbeb29
jcstein <46639943+jcstein@users.noreply.github.com>
2025-10-03 08:35:53 -0600

    docs: add new arabica versions Arabica: celestia-node v0.27.5-arabica and celestia-app v6.1.0-arabica upgrade

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v6.0.5-arabica",
-  "app-latest-sha": "6d05e9ea9ad801cb717df404bc50f65583cc2033",
+  "app-latest-tag": "v6.1.0-arabica",
+  "app-latest-sha": "9ea7c3e02726f105a1ccff1b7141b266d83fff8b",
   "node-latest-tag": "v0.27.5-arabica",
   "node-latest-sha": "e6a940b6c222a9f73590462329fa4a1a3c1e2d3e",
 });
