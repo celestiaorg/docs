203f17e1d79b975ec0479888bb2685c3a1d0013f
jcstein <46639943+jcstein@users.noreply.github.com>
2025-10-06 13:14:55 -0600

    chore: update mainnet beta to celestia-node v0.26.4

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -1,7 +1,7 @@
 const mainnetVersions = Object.freeze({
   "app-latest-tag": "v5.0.5",
   "app-latest-sha": "bb1b85dea2aba0228b4502f02f0c7ee9d7014ab7",
-  "node-latest-tag": "v0.25.3",
-  "node-latest-sha": "8bee349fb74ebb8fdcbe68fad4c9dae750a91807",
+  "node-latest-tag": "v0.26.4",
+  "node-latest-sha": "1b84e2b3f0df39a6f2197096fb2a58d9cd250687",
 });
 export default mainnetVersions;
