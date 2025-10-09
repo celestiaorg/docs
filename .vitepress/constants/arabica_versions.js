8042162f0ca33aa093ed020f796c10c16d04e6c8
jcstein <46639943+jcstein@users.noreply.github.com>
2025-10-09 11:14:56 -0600

    chore: upgrade arabica to v0.28.0-arabica

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
   "app-latest-tag": "v6.1.1-arabica",
   "app-latest-sha": "e45d6ecce15cf3e8fa3ef9602ec539f739412fe3",
-  "node-latest-tag": "v0.27.5-arabica",
-  "node-latest-sha": "e6a940b6c222a9f73590462329fa4a1a3c1e2d3e",
+  "node-latest-tag": "v0.28.0-arabica",
+  "node-latest-sha": "73d132de14fe3379d53b38f6aec35c4b8b5c1dd2",
 });
 export default arabicaVersions;
