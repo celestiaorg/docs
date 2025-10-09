dca9250090d407df7b0551b09ba4e922d4a29bb9
jcstein <46639943+jcstein@users.noreply.github.com>
2025-10-09 07:38:57 -0600

    docs: add v6.1.1-arabica

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v6.1.0-arabica",
-  "app-latest-sha": "9ea7c3e02726f105a1ccff1b7141b266d83fff8b",
+  "app-latest-tag": "v6.1.1-arabica",
+  "app-latest-sha": "e45d6ecce15cf3e8fa3ef9602ec539f739412fe3",
   "node-latest-tag": "v0.27.5-arabica",
   "node-latest-sha": "e6a940b6c222a9f73590462329fa4a1a3c1e2d3e",
 });
