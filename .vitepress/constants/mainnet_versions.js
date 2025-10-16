6af4a19b24218def9e64f0a06aa298ac79c5bc3a
Luke Cassady-Dorion <luke@SPStories.com>
2025-10-16 22:53:52 +0700

    Update mainnet_versions.js
    Updated app hash to 5.0.11
    
    4e34cf6bac0bc9c7a3be796470e6f03cd7b34546

--- .vitepress/constants/mainnet_versions.js
+++ .vitepress/constants/mainnet_versions.js
@@ -1,6 +1,6 @@
 const mainnetVersions = Object.freeze({
-  "app-latest-tag": "v5.0.5",
-  "app-latest-sha": "bb1b85dea2aba0228b4502f02f0c7ee9d7014ab7",
+  "app-latest-tag": "v5.0.11",
+  "app-latest-sha": "4e34cf6bac0bc9c7a3be796470e6f03cd7b34546",
   "node-latest-tag": "v0.26.4",
   "node-latest-sha": "1b84e2b3f0df39a6f2197096fb2a58d9cd250687",
 });
