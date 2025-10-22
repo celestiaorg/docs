a6382fb20e360de24914ce1ee2ba8da803ac0e76
jcstein <z@zs-MacBook-Air.local>
2025-10-22 08:01:29 -0600

    docs: v6.2.0-arabnica

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,6 +1,6 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v6.1.2-arabica",
-  "app-latest-sha": "bd8fc305106a9af4624ee9ca79a9479dc226bb92",
+  "app-latest-tag": "v6.2.0-arabica",
+  "app-latest-sha": "00f1405a9dbeb0408fa639c47ce93fcd0610ffdb",
   "node-latest-tag": "v0.28.1-arabica",
   "node-latest-sha": "df2e63b84e2db8d3c2fbd380168c4a1f7be42b92",
 });
