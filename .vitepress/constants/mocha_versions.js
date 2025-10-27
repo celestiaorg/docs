21e81303ddc4cdbb7625c405116e786462011c96
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-10-27 09:56:56 -0600

    docs: v6.2.0-mocha

--- .vitepress/constants/mocha_versions.js
+++ .vitepress/constants/mocha_versions.js
@@ -1,6 +1,6 @@
 const mochaVersions = Object.freeze({
-  "app-latest-tag": "v6.0.5-mocha",
-  "app-latest-sha": "6d05e9ea9ad801cb717df404bc50f65583cc2033",
+  "app-latest-tag": "v6.2.0-mocha",
+  "app-latest-sha": "0bf64719e49d61ee95135479fcc8d573a92f7662",
   "node-latest-tag": "v0.28.2-mocha",
   "node-latest-sha": "e4384c0f4b5de00d65e321ee9f4f400850165e6d",
 });
