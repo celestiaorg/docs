05dffba8349cbbd25b8c340ce238850bbd3f38d2
Luke Cassady-Dorion <luke@SPStories.com>
2025-11-06 23:48:35 +0700

    Adds IBC overview and reworks menu structure moving items from BUILD to LEARN (#2294)

---
+++ app/learn/TIA/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  "overview": "Overview",
+  "governance": "Governance",
+  "defi": "DeFi",
+};
---
+++ app/learn/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  "celestia-101": "Celestia 101",
+  features: "Features",
+  bridging: "Bridging",
+  TIA: "TIA",
+};
