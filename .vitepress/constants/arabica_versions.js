726bd5289984e9f5d6283982f2b4019ffe8cf396
Josh Stein <46639943+jcstein@users.noreply.github.com>
2025-11-13 10:14:12 -0700

    docs: add new versions for v6.2.5 (#2305)
    * docs: add new versions for v6.2.5
    
    * docs: add arabica new
    
    ---------
    
    Co-authored-by: jcstein <z@zs-MacBook-Air.local>

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
-  "app-latest-tag": "v6.2.2-arabica",
-  "app-latest-sha": "372053e8251f77b5e6c343d49a195989889f2e29",
-  "node-latest-tag": "v0.28.2-arabica",
-  "node-latest-sha": "c940ee5800b31ccf927df2195a6c7b1e24385f52",
+  "app-latest-tag": "v6.2.5-arabica",
+  "app-latest-sha": "7b98de1e3bbb196cb358825220bf4a8f8fc4c81b",
+  "node-latest-tag": "v0.28.3-arabica",
+  "node-latest-sha": "72ca378cd924772e96ea9537352572a25b8996ca",
 });
 export default arabicaVersions;
