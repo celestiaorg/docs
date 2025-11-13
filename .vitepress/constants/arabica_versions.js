3c9600eed7fea1d26053f726772af4dfe697cffd
jcstein <z@zs-MacBook-Air.local>
2025-11-13 09:33:46 -0700

    docs: add arabica new

--- .vitepress/constants/arabica_versions.js
+++ .vitepress/constants/arabica_versions.js
@@ -1,7 +1,7 @@
 const arabicaVersions = Object.freeze({
   "app-latest-tag": "v6.2.5-arabica",
   "app-latest-sha": "7b98de1e3bbb196cb358825220bf4a8f8fc4c81b",
-  "node-latest-tag": "v0.28.2-arabica",
-  "node-latest-sha": "c940ee5800b31ccf927df2195a6c7b1e24385f52",
+  "node-latest-tag": "v0.28.3-arabica",
+  "node-latest-sha": "72ca378cd924772e96ea9537352572a25b8996ca",
 });
 export default arabicaVersions;
