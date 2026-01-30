196d8816562c7e0259f4eea4c72f515a97024c66
Josh Stein <46639943+jcstein@users.noreply.github.com>
2026-01-30 12:59:09 -0700

    docs: move around existing contents into 1 dir

--- app/build/stacks/optimism/_meta.js
+++ app/build/stacks/optimism/_meta.js
@@ -2,6 +2,7 @@ const meta = {
   "op-alt-da": "OP alt DA",
   "popkins-popsigner":
     "Deploying OP alt DA with Popkins, Popsigner, and Docker",
+  "aws-kms-guide": "AWS KMS guide",
   "local-devnet": "Local OP Stack Devnet",
 };
 
