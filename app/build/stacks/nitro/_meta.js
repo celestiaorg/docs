7715111883505851b165f4eb7a4a862042efe2f8
Josh Stein <46639943+jcstein@users.noreply.github.com>
2026-02-11 19:14:47 -0700

    docs: add nitro guide for popsigner

--- app/build/stacks/nitro/_meta.js
+++ app/build/stacks/nitro/_meta.js
@@ -1,6 +1,7 @@
 const meta = {
   "nitro-das-server": "Nitro DAS server overview",
   "local-devnet": "Nitro local devnet",
+  "popkins-popsigner": "Deploy with Popkins and Popsigner",
 };
 
 export default meta;
